fc896cd9dcffe56367fcf78e993aa87435d0c744
zl2006 <zl2006@gmail.com>
2015-12-31 17:34:41 +0800

    变更pom

--- framework-util/src/main/java/org/yy/framework/util/net/FtpUtils.java
+++ framework-util/src/main/java/org/yy/framework/util/net/FtpUtils.java
@@ -1,21 +1,21 @@
- /*
- * 文 件 名:  FtpUtils.java
- * 版    权:  YY Technologies Co., Ltd. Copyright 2012-2013,  All rights reserved
- * 描    述:  <描述>
- * 修 改 人:  zhouliang
- * 修改时间:  2015年12月23日
- * 修改内容:  <修改内容>
- */
+/*
+* 文 件 名:  FtpUtils.java
+* 版    权:  YY Technologies Co., Ltd. Copyright 2012-2013,  All rights reserved
+* 描    述:  <描述>
+* 修 改 人:  zhouliang
+* 修改时间:  2015年12月23日
+* 修改内容:  <修改内容>
+*/
 package org.yy.framework.util.net;
 
- /**
- * <一句话功能简述>
- * <功能详细描述>
+/**
+ * FTP工具类
  * 
- * @author  zhouliang
- * @version  [版本号, 2015年12月23日]
- * @since  [产品/模块版本]
+ * @author zhouliang
+ * @version [1.0, 2015年12月24日]
+ * @since [framework-util/1.0]
  */
-public class FtpUtils {
-    
+public final class FtpUtils {
+	private FtpUtils() {
+	}
 }
